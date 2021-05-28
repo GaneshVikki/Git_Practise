93e22da932cbb2b0ad8ae3183af79b7dc9f9f6b1
Ganesh R <ganesh.ramamoorthy@tagitmobile.com>
2021-05-28 13:20:25 +0530

    Second Commit

---
+++ DemoOne/src/com/test/demo/FirstDemo.java
@@ -0,0 +1,11 @@
+package com.test.demo;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First test");
+
+	}
+
+}
---
+++ DemoOne/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Tagit
+ *
+ */
+module Demo {
+}
